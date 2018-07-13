0ec2b89d7073dd82e4a2b15e8f06c2d56effbe82
James Pirruccello <jamesp@broadinstitute.org>
2018-07-13 11:45:24 -0400

    Implementes BGEN 1.3 / ZStandard support.

--- zstd.go
+++ zstd.go
@@ -1,10 +1,6 @@
 package bgen
 
-import (
-	"fmt"
-
-	"github.com/carbocation/pfx"
-)
+import "github.com/DataDog/zstd"
 
 // DecompressZStandard moves the ZStandard decompression into its own file to
 // facilitate separating ZStandard compatibility (which requires cgo) into its
@@ -12,5 +8,5 @@ import (
 // buffer to use, you can pass it to prevent allocation. If it is too small, or
 // if nil is passed, a new buffer will be allocated and returned."
 func DecompressZStandard(dst, src []byte) ([]byte, error) {
-	return nil, pfx.Err(fmt.Errorf("To use ZStandard (BGEN format 1.3), please use the bgen13 branch"))
+	return zstd.Decompress(dst, src)
 }

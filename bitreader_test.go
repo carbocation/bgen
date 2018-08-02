525574ebef0fe3a6aefec4eaa51478c3ef354e21
James Pirruccello <jamesp@broadinstitute.org>
2018-08-01 22:28:25 -0400

    Replaced bitreader with a version that yields the correct answers.

--- bitreader_test.go
+++ bitreader_test.go
@@ -1,7 +1,6 @@
 package bgen
 
 import (
-	"bytes"
 	"encoding/binary"
 	"testing"
 )
@@ -13,46 +12,10 @@ func TestBitReader(t *testing.T) {
 	binary.LittleEndian.PutUint64(data, target)
 
 	val := 0
-	br := newBitReader(bytes.NewBuffer(data))
+	br := newBitReader(data, 7)
 	for i := 0; i < len(data); i++ {
-		var bit uint
-		truth, err := br.ReadBit()
-		if err != nil {
-			t.Fatal(err)
-		}
-		if truth {
-			bit = 1
-		}
+		bit := br.getBit(i)
 		val |= 1 << bit
-	}
-
-	if target != uint64(val) {
-		t.Errorf("Got %d, expected %d", val, target)
-	}
-}
-
-func TestBitReadUint(t *testing.T) {
-	var target uint64 = 3
-	data := make([]byte, 8) // Big enough to hold a uint64
-
-	binary.LittleEndian.PutUint64(data, target)
-
-	var val uint64
-	br := newBitReader(bytes.NewBuffer(data))
-
-	val, err := br.ReadUint(8)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if target != uint64(val) {
-		t.Errorf("Got %d, expected %d", val, target)
-	}
-
-	br = newBitReader(bytes.NewBuffer(data))
-	val, err = br.ReadUintLittleEndian(8)
-	if err != nil {
-		t.Error(err)
 	}
 
 	if target != uint64(val) {
@@ -63,35 +26,25 @@ func TestBitReadUint(t *testing.T) {
 func TestBitReaderLittleEndian7Bit(t *testing.T) {
 	value := []byte{93}
 
-	br := newBitReader(bytes.NewBuffer(value))
-	valBig, err := br.ReadUint(7)
-	if err != nil {
-		t.Error(err)
-	}
+	br := newBitReader(value, 7)
+	var valBig uint32
+	br.Next(&valBig)
 
-	br = newBitReader(bytes.NewBuffer(value))
-	valLittle, err := br.ReadUintLittleEndian(7)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if valBig != valLittle {
-		t.Errorf("First 7 bits of %d yielded %d from bigendian, different from %d from littleendian", value[0], valBig, valLittle)
+	if valBig != 93 {
+		t.Errorf("First 7 bits of %d yielded %d, expected %d", value[0], valBig, 93)
 	}
 }
 
 func TestBitReaderLittleEndian16Bit(t *testing.T) {
 	value := []byte{93, 115}
 
-	br := newBitReader(bytes.NewBuffer(value))
-	valLittle, err := br.ReadUintLittleEndian(16)
-	if err != nil {
-		t.Error(err)
-	}
+	br := newBitReader(value, 16)
+	var valLittle uint32
+	br.Next(&valLittle)
 
 	properLittleEndian := binary.LittleEndian.Uint16(value)
 
-	if valLittle != uint64(properLittleEndian) {
+	if valLittle != uint32(properLittleEndian) {
 		t.Errorf("First 12 bits of %016b\n yielded %016b from bigendian,\n different from %016b from littleendian", value, properLittleEndian, valLittle)
 	}
 }

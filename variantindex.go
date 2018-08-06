89e42bbea2ef839e68c7cf564203f70848a2f43e
James Pirruccello <jamesp@broadinstitute.org>
2018-08-06 18:21:01 -0400

    Move BGENTime into its own file.

--- variantindex.go
+++ variantindex.go
@@ -1,9 +1,6 @@
 package bgen
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/jmoiron/sqlx"
 )
 
@@ -50,36 +47,8 @@ type VariantIndex struct {
 // "Metadata" from more recent versions of BGEN.
 type BGIMetadata struct {
 	Filename           string
-	FileSize           uint     `db:"file_size"`
-	LastWriteTime      BGENTime `db:"last_write_time"`
-	FirstThousandBytes []byte   `db:"first_1000_bytes"`
-	IndexCreationTime  BGENTime `db:"index_creation_time"`
-}
-
-// BGENTime exists to facilitate time parsing from the Metadata, because BGEN
-// uses both unixtime and text strings to represent time. Derived from
-// https://github.com/mattn/go-sqlite3/issues/190#issuecomment-343341834f
-type BGENTime time.Time
-
-func (t *BGENTime) Scan(v interface{}) error {
-	switch which := v.(type) {
-	case int64:
-		vt := time.Unix(which, 0)
-		*t = BGENTime(vt)
-		return nil
-	case int:
-		vt := time.Unix(int64(which), 0)
-		*t = BGENTime(vt)
-		return nil
-	case []byte:
-		// Should be more strictly to check this type.
-		vt, err := time.Parse("2006-01-02 15:04:05", string(which))
-		if err != nil {
-			return err
-		}
-		*t = BGENTime(vt)
-		return nil
-	}
-
-	return fmt.Errorf("No appropriate type could be found to decode %v", v)
+	FileSize           uint   `db:"file_size"`
+	LastWriteTime      Time   `db:"last_write_time"`
+	FirstThousandBytes []byte `db:"first_1000_bytes"`
+	IndexCreationTime  Time   `db:"index_creation_time"`
 }

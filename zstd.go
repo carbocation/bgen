ab4b244a8697aa10c84e3507472dcbb9eb6a36ff
James Pirruccello <jamesp@broadinstitute.org>
2019-09-04 16:46:21 -0400

    Using klauspost's pure-go ZStandard decoder. However, I have no tests for Zstandard, so this is not guaranteed to work.

--- zstd.go
+++ zstd.go
@@ -1,11 +1,18 @@
 package bgen
 
-import "github.com/DataDog/zstd"
+import (
+	"github.com/klauspost/compress/zstd"
+)
 
 // DecompressZStandard decompresses Zstd compressed data for bgen13. As per the
 // original, "Decompress src into dst. If you have a buffer to use, you can pass
 // it to prevent allocation. If it is too small, or if nil is passed, a new
 // buffer will be allocated and returned."
 func DecompressZStandard(dst, src []byte) ([]byte, error) {
-	return zstd.Decompress(dst, src)
+	dec, err := zstd.NewReader(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return dec.DecodeAll(src, dst)
 }
